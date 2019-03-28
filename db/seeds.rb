8e05204cb67f9ea06b91e456ec05f04d4b1323d5
Derek Johnson <wars2cool@gmail.com>
2019-03-28 15:23:55 +0900

    routes and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+Dose.destroy_all
+Ingredient.destroy_all
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredient_list = open(url).read
+parsed_ingredient_list = JSON.parse(ingredient_list)
+parsed_ingredient_list["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
