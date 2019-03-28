7fde9202d7bc24ab6ff9b3205e1adb53d9b41417
Derek Johnson <wars2cool@gmail.com>
2019-03-28 17:44:15 +0900

    routes fixes, views basic

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,25 +1,29 @@
 class DosesController < ApplicationController
   def new
-    @cocktail = Cocktail.find(params[:restaurant_id])
-    @ingredient = Ingredient.find(params[:ingredient_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(review_params)
-    @dose.cocktail = Cocktail.find(params[:restaurant_id])
-    @dose.ingredient = Ingredient.find(params[:restaurant_id])
-    @dose.save
-    #redirect_to restaurant_path(@review.restaurant)
+        @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else
+      render :new
+    end
   end
 
   def destroy
-    dose.destroy
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(cocktail)
   end
 
   private
 
   def review_params
-    params.require(:dose).permit(:amount, :description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktail, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create, :edit] do
+    resources :doses, only: [:new, :create, :destroy]
   end
-  delete '/doses/:id', to: 'doses#destroy', as: 'dose'
   root 'cocktails#index'
 end
